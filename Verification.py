c9a6192e00903dff59cbc5d708fe1d122bcf9163
Pauonpu <pau.ondono@estudiantat.upc.edu>
2023-11-26 09:46:08 +0100

    update

--- Verification.py
+++ Verification.py
@@ -1,7 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 from scipy import integrate
 import Parameters as par
 import Vortex_Iteration as vi
+
+
 
 th_p = np.arccos(1 - 2 * par.p)
 
@@ -10,7 +14,7 @@ def dz(th):
     if th < th_p:
         z = (par.f / par.p**2) * (2 * par.p - 1 + np.cos(th))
     elif th_p <= th:
-        z = (par.f / (1 - par.p**2)) * (2 * par.p - 1 + np.cos(th))
+        z = (par.f / (1 - par.p)**2) * (2 * par.p - 1 + np.cos(th))
     return z
 
 def int1(th):
@@ -51,3 +55,53 @@ if par.eta>0:
 
 CM_tat=CM0_tat-CL_tat/4
 
+####################
+
+#Ara trobem valors de Cl i CM0 mitjançant DVM per N panells (entre 1 i 400)
+
+def DVM(i):
+    # Replace this with your DVM implementation
+    CL = 0.7
+    CM = 0.3
+    return CL, CM
+
+M = 400  # Maximum number of panels
+data = np.zeros((3, M))
+
+for i in range(1, M + 1):
+    start_time = time.time()
+    CL, CM = DVM(i)
+    data[0, i - 1] = CL
+    data[1, i - 1] = CM
+    data[2, i - 1] = time.time() - start_time
+
+error = np.zeros((2, M))
+error[0, :] = np.abs((data[0, :] - CL_tat) / CL_tat) * 100
+error[1, :] = np.abs((data[1, :] - CM_tat) / CM_tat) * 100
+
+# Plots: elapsed time, CL, CM
+plt.figure(figsize=(10, 6))
+
+# Elapsed time plot
+plt.subplot(3, 1, 1)
+plt.plot(data[2, :], label='Elapsed Time')
+plt.xlabel('Panel Number')
+plt.ylabel('Elapsed Time (s)')
+plt.legend()
+
+# CL convergence plot
+plt.subplot(3, 1, 2)
+plt.plot(data[0, :], label='CL')
+plt.xlabel('Panel Number')
+plt.ylabel('CL')
+plt.legend()
+
+# CM convergence plot
+plt.subplot(3, 1, 3)
+plt.plot(data[1, :], label='CM')
+plt.xlabel('Panel Number')
+plt.ylabel('CM')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
