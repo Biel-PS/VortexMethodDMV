c1e3445054ca3e46c9ef0f77c4ca407e14d329f9
Miquel <miquel.tomas.pala@estudiantat.upc.edu>
2023-11-28 18:01:42 +0100

    update graf alfa

--- Verification.py
+++ Verification.py
@@ -70,7 +70,7 @@ par.Parameters_definition()
 cont = 0
 
 start =1
-finish = 200
+finish = 400
 step = 1
 lenght = np.abs(start/step)+np.abs(finish/step) + 1
 
@@ -83,7 +83,7 @@ Cmxh = np.zeros((int(lenght),1))
 print('|Panels number|','|Cl perfil|','|Cmle|')
 
 
-data = np.zeros((3, finish))
+data = np.zeros((3, 400))
 
 for i in range(start,finish+step,step):
     start_time = time.time()
@@ -145,3 +145,182 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
+
+
+
+"""
+def DVM(i):
+    # Replace this with your DVM implementation
+    CL = 0.7
+    CM = 0.3
+    return CL, CM
+
+
+M = 400  # Maximum number of panels
+data = np.zeros((3, M))
+
+
+for i in range(1, M + 1):
+    start_time = time.time()
+    CL = vi.Lift_Coeficient(circulation, infopanel)[0]
+    CM = vi.MomentLE_Coeficient(circulation, infopanel)[0]
+    data[0, i - 1] = CL
+    data[1, i - 1] = CM
+    data[2, i - 1] = time.time() - start_time
+
+error = np.zeros((2, M))
+error[0, :] = np.abs((data[0, :] - CL_tat) / CL_tat) * 100
+error[1, :] = np.abs((data[1, :] - CM_tat) / CM_tat) * 100
+
+# Plots: elapsed time, CL, CM
+plt.figure(figsize=(10, 6))
+
+# Elapsed time plot
+plt.subplot(3, 1, 1)
+plt.plot(data[2, :], label='Elapsed Time')
+plt.xlabel('Panel Number')
+plt.ylabel('Elapsed Time (s)')
+plt.legend()
+
+# CL convergence plot
+plt.subplot(3, 1, 2)
+plt.plot(data[0, :], label='CL')
+plt.xlabel('Panel Number')
+plt.ylabel('CL')
+plt.legend()
+
+# CM convergence plot
+plt.subplot(3, 1, 3)
+plt.plot(data[1, :], label='CM')
+plt.xlabel('Panel Number')
+plt.ylabel('CM')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+"""
+######################################################################
+p_range = np.linspace(0.1,0.9,10)
+M = 10
+th_pp = []
+"""for i in range(1, M + 1):
+    print(i)
+    th_pp[i-1] = np.arccos(1 - 2 * p_range[i-1])
+    print(th_pp[i-1])
+print(th_pp)
+"""
+valvec = np.zeros(10)
+mi_vector = np.zeros(10)
+alfa_vector = np.zeros((4, 10))
+for j in range(4):
+    f_bucle = j * 0.02
+    print(f_bucle)
+    for i in range(10):
+        valor = 0.1 + i * 0.0588888
+        th_pp = np.arccos(1 - 2 * valor)
+        print(i)
+        def dz(th):
+            if th < th_pp:
+                z = (f_bucle / valor ** 2) * (2 * valor - 1 + np.cos(th))
+            elif th_pp <= th:
+                z = (f_bucle / (1 - valor) ** 2) * (2 * valor - 1 + np.cos(th))
+            return z
+        result3, error3 = integrate.quad(int3, 0, np.pi)
+        alfa_lo_tat = -1 / np.pi * result3
+        mi_vector[i] = alfa_lo_tat * 180 / np.pi
+        valvec[i] = valor
+    alfa_vector[j] = mi_vector
+print("alfa", alfa_vector)
+
+"""
+plt.figure(figsize=(10, 6))
+plt.plot(mi_vector, label='p')
+plt.xlabel('P')
+plt.ylabel('alfa')
+"""
+
+
+
+
+# Supongamos que tienes un vector de valores
+
+
+# Crear el gráfico de líneas
+aaa = 0
+for fila in alfa_vector:
+    aaa = aaa + 1
+    plt.plot(valvec, fila, linestyle='-', label=f'f = {0.02*(aaa-1)}')
+#plt.plot(valvec, alfa_vector, marker='o', linestyle='-', color='b', label='Mi Vector')
+
+# Añadir etiquetas a los ejes y al gráfico
+plt.xlabel('p')
+plt.ylabel('alfa_lo')
+plt.title('Angle zero lift en funció de p i f')
+plt.legend()  # Mostrar la leyenda si se especifican etiquetas en la función plot
+plt.grid(True)
+plt.xlim(0.1, 0.6)
+# Mostrar el gráfico
+plt.show()
+######################################MIQUEL#######################################
+######################################################################
+p_range = np.linspace(0.1,0.9,10)
+M = 10
+th_pp = []
+"""for i in range(1, M + 1):
+    print(i)
+    th_pp[i-1] = np.arccos(1 - 2 * p_range[i-1])
+    print(th_pp[i-1])
+print(th_pp)
+"""
+valvec = np.zeros(10)
+mi_vector = np.zeros(10)
+alfa_vector = np.zeros((4, 10))
+for j in range(4):
+    f_bucle = j * 0.02
+    print(f_bucle)
+    for i in range(10):
+        valor = 0.1 + i * 0.0588888
+        th_pp = np.arccos(1 - 2 * valor)
+        print(i)
+        def dz(th):
+            if th < th_pp:
+                z = (f_bucle / valor ** 2) * (2 * valor - 1 + np.cos(th))
+            elif th_pp <= th:
+                z = (f_bucle / (1 - valor) ** 2) * (2 * valor - 1 + np.cos(th))
+            return z
+        result3, error3 = integrate.quad(int3, 0, np.pi)
+        alfa_lo_tat = -1 / np.pi * result3
+        mi_vector[i] = alfa_lo_tat * 180 / np.pi
+        valvec[i] = valor
+    alfa_vector[j] = mi_vector
+print("alfa", alfa_vector)
+
+"""
+plt.figure(figsize=(10, 6))
+plt.plot(mi_vector, label='p')
+plt.xlabel('P')
+plt.ylabel('alfa')
+"""
+
+
+
+
+# Supongamos que tienes un vector de valores
+
+
+# Crear el gráfico de líneas
+aaa = 0
+for fila in alfa_vector:
+    aaa = aaa + 1
+    plt.plot(valvec, fila, linestyle='-', label=f'f = {0.02*(aaa-1)}')
+#plt.plot(valvec, alfa_vector, marker='o', linestyle='-', color='b', label='Mi Vector')
+
+# Añadir etiquetas a los ejes y al gráfico
+plt.xlabel('p')
+plt.ylabel('alfa_lo')
+plt.title('Angle zero lift en funció de p i f')
+plt.legend()  # Mostrar la leyenda si se especifican etiquetas en la función plot
+plt.grid(True)
+plt.xlim(0.1, 0.6)
+# Mostrar el gráfico
+plt.show()
