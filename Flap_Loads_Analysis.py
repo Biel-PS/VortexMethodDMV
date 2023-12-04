bfb8142396323e9ed8de48bef2281032ba9d1862
pauuandress <pau.andres@estudiantat.upc.edu>
2023-12-04 11:22:20 +0100

    update

--- Flap_Loads_Analysis.py
+++ Flap_Loads_Analysis.py
@@ -9,7 +9,7 @@ coord = np.zeros((N+1, 2))  # filas, columnas; x y
 
 
 # Lista de valores de x_h para iterar
-x_h_values = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]  # Puedes ajustar esta lista según tus necesidades
+x_h_values = [0.7, 0.75, 0.8, 0.85]  # Puedes ajustar esta lista según tus necesidades
 print('|Angle [deg]|', '|Cl perfil|', '|Delta Cl flap|', '|Cmle|', '|Cmxh|', '|xh|')
 for x_h in x_h_values: #recorre los diferentes valores de xh
     cont = 0
