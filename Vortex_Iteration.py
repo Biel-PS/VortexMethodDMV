b2c682bab55d60e8256a39f5a241e341d8b3b659
Biel Pujadas Suriol <bielpujadassuriol@gmail.com>
2023-12-06 01:04:12 +0100

    Update

--- Vortex_Iteration.py
+++ Vortex_Iteration.py
@@ -5,6 +5,7 @@ import Parameters as par
 def Calc_coord_UNIFORM (cordMatrix,p,N,xh,eta): #Create the node points using a linal aprixmation
     cont = 0
     step = 1/N
+    angleMatrix = [[np.cos(par.eta), np.sin(par.eta)], [-np.sin(par.eta), np.cos(par.eta)]]
     for x in np.arange(0.0,1.000001,step):
         if x < p: #first half of the naca parabola
             cordMatrix[cont,0] = x
@@ -15,27 +16,32 @@ def Calc_coord_UNIFORM (cordMatrix,p,N,xh,eta): #Create the node points using a 
         cont +=1
     for i in range(0,len(cordMatrix)): #We rotate the points from the flap the deflection angle
         if cordMatrix[i][0]>= xh and eta !=0:
-            cordMatrix[i] = np.matmul([[np.cos(par.eta), np.sin(par.eta)], [-np.sin(par.eta), np.cos(par.eta)]],
+            cordMatrix[i] = np.matmul(angleMatrix,
                                       [cordMatrix[i][0] - xh, cordMatrix[i][1]]) + [xh, 0]
 def Calc_coord_Cosinus (cordMatrix,p,N,xh,eta): #Create the node points using an angular cosinus aproximation
+    angleMatrix = [[np.cos(par.eta),np.sin(par.eta)],[-np.sin(par.eta),np.cos(par.eta)]]
     cont = 0
     Control = True
+
+    Fa1 = (par.f/par.p**2)
+    Fa2 = (par.f/  (1-par.p) ** 2 )
+
     while Control:
-        x = 0.5*(1-np.cos(np.pi*(cont)/(N)))
+        x = 0.5*(1-np.cos(np.pi*((cont)/(N))))
         if x < p: #First parabola deffined by the naca 4 digit standard
             cordMatrix[cont,0] = x
-            cordMatrix[cont,1] = (par.f/par.p**2)*(2*par.p*x-x**2)
+            cordMatrix[cont,1] =  Fa1 * (2*par.p*x - x**2)
         elif x >= p and x<0.99999999:#second parabola deffined by the naca 4 digit standard
             cordMatrix[cont,0] = x
-            cordMatrix[cont,1] = (par.f/(1-par.p)**2)*((1-2*par.p)+2*par.p*x-x**2)
+            cordMatrix[cont,1] =  Fa2 * ((1 - 2 * par.p) + 2 * par.p * x - x**2)
         else: #Last node is TE of the airfoil
             cordMatrix[cont, 0] = 1
-            cordMatrix[cont, 1] = (par.f / (1 - par.p)**2) * ((1 - 2 * par.p) -1 + 2 * par.p )
+            cordMatrix[cont, 1] = Fa2 * ((1 - 2 * par.p) - 1 + 2 * par.p )
             Control = False
         cont += 1
     for i in range(0,len(cordMatrix)):#We rotate the points from the flap the deflection angle
         if cordMatrix[i][0] >= xh and eta !=0:
-            cordMatrix[i] = np.matmul([[np.cos(par.eta),np.sin(par.eta)],[-np.sin(par.eta),np.cos(par.eta)]],[cordMatrix[i][0]-xh,cordMatrix[i][1]]) + [xh,0]
+            cordMatrix[i] = np.matmul(angleMatrix,[cordMatrix[i][0]-xh,cordMatrix[i][1]]) + [xh,0]
 
 def Calc_panel (cordMatrix,N): #Define the panel as a plain segmen between two nodes
     cont_i = 0 # we define the cache that will be used in the definition of the panels
@@ -47,24 +53,24 @@ def Calc_panel (cordMatrix,N): #Define the panel as a plain segmen between two n
         if cont_k != 0: # we dont need to operate with the first point of the extradros
             for k in i:
                 if cont_i == 0: #for the x coordinate of a point
-                    delta_x = cordMatrix[cont_k,cont_i] - cache_x
+                    delta_x = k - cache_x
                     xo = cache_x
-                    cache_x = cordMatrix[cont_k,cont_i]
+                    cache_x = k
                     cont_i = 1
                 else: #for the z coordinate of a point
-                    delta_z = cordMatrix[cont_k, cont_i] - cache_z
+                    delta_z = k - cache_z
                     zo = cache_z
-                    cache_z = cordMatrix[cont_k, cont_i]
+                    cache_z = k
                     cont_i = 0
 
-            panel_chord = np.sqrt(delta_z**2 + delta_x**2) # We define the length of the panel i functon of the total airfoil chord
+            panel_chord = np.hypot(delta_z,delta_x) # We define the length of the panel i functon of the total airfoil chord (sqrt(deltaz^2 + delta_x^2))
 
             vec_normal = (-delta_z/panel_chord,delta_x/panel_chord) #vector normal to the segment
 
             vec_tangent = (delta_x/panel_chord,delta_z/panel_chord)#vector tangent to the segment
 
-            pos_lumpedVortex = [xo + (0.25*panel_chord)*vec_tangent[0],vec_tangent[1]+zo] #postion of the vortex in one segment (0,25 of the panel lenght)
-            pos_controlpoint = [xo + (0.75 * panel_chord)*vec_tangent[0],vec_tangent[1]+zo] #postion of the control point of the segment (0,75 of the panel lenght)
+            pos_lumpedVortex = [xo + (0.25*panel_chord)*vec_tangent[0],zo + (0.25*panel_chord)*vec_tangent[1]] #postion of the vortex in one segment (0,25 of the panel lenght)
+            pos_controlpoint = [xo + (0.75 * panel_chord)*vec_tangent[0],zo + (0.75*panel_chord)*vec_tangent[1]] #postion of the control point of the segment (0,75 of the panel lenght)
 
             conjunto = np.array([np.transpose(vec_normal),np.transpose(vec_tangent),pos_lumpedVortex,pos_controlpoint]) #we set all the data to an array
             property_panel.append(conjunto) #create a matrix with all the paramenters of every panel appending the created array
