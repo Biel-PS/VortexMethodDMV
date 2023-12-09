00d50530411f07b444212fa9213c5f26929ac74a
pauuandress <pau.andres@estudiantat.upc.edu>
2023-12-09 18:28:38 +0100

    update

--- Flap_Loads_Analysis.py
+++ Flap_Loads_Analysis.py
@@ -9,27 +9,24 @@ coord = np.zeros((N+1, 2))  # filas, columnas; x y
 
 
 # Lista de valores de x_h para iterar
-x_h_values = [0.3, 0.25, 0.2, 0.15]
-par.Parameters_definition()
+x_h_values = [0.7, 0.75, 0.8, 0.85]
 print('|Angle [deg]|', '|Cl perfil|', '|Delta Cl flap|', '|Cmle|', '|Cmxh|', '|xh|')
 for x_h in x_h_values: #recorre los diferentes valores de xh
-    par.alfa = -2 * (np.pi / 180)
+    alfa = -16.525597751298992 * (np.pi / 180)
     cont = 0
     eta_inicial = 0
-    eta_final = 20
+    eta_final = 15
     step = 1
     lenght = np.abs(eta_inicial/step) + np.abs(eta_final/step) + 1
     angle = np.zeros((int(lenght), 1))
     hinge_pos = x_h
-    par.xh = hinge_pos
     Cl = np.zeros((int(lenght), 1))  # Cl con flap del ala
     Cl_flap = np.zeros((int(lenght), 1))  # Cl del flap
     Cmle = np.zeros((int(lenght), 1))  # coef de momentos del perfil respecto borde de ataque
     Cmxh = np.zeros((int(lenght), 1))  # coef de momentos del flap respecto eje de charnela
     for i in range(eta_inicial, eta_final + step, step):
-         # definimos los parámetros
+        par.Parameters_definition()  # definimos los parámetros
         par.eta = i * (np.pi / 180)  # convertimos el ángulo a radianes
-
         vi.Calc_coord_Cosinus(coord, par.p, N, x_h, i)
         # calculamos con el ángulo de 0
         infoMatrix = vi.Calc_panel(coord, N)  # VECTOR NORMAL, VECTOR TANGENTE, X LUMPED VORTEX, X CONTROL POINT
@@ -46,7 +43,7 @@ for x_h in x_h_values: #recorre los diferentes valores de xh
     plt.plot(angle, Cl, label=f'x_h = {x_h}')
 
 # Configuración del gráfico
-plt.title('Coeficiente de Sustentación (Cl) vs. Ángulo para diferentes valores de x_h')
+plt.title('Cl y Cm_LE vs. Ángulo de deflección')
 plt.xlabel('Ángulo (grados)')
 plt.ylabel('Cl')
 plt.legend()
